a628669b5d055b42b8c4e01f74b7ded2778aa627
Linnea <89969399+linneaha@users.noreply.github.com>
2023-01-16 13:17:28 +0100

    big commit

--- backend/app.js
+++ backend/app.js
@@ -1,19 +1,32 @@
 import express from "express";
 import mongoose from "mongoose";
+import cors from "cors";
+import bodyParser from "body-parser";
 
 const app = express();
-app.use(express.json());
 
+// middleware
+app.use(express.json());
+app.use(cors());
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+
+// databas
 mongoose.set("strictQuery", false);
 const PORT = 3001 || 6001;
 mongoose
-  .connect( "mongodb+srv://dummyUser:dummyPassword123@cluster0.5skhm3x.mongodb.net/?retryWrites=true&w=majority", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(
+    "mongodb+srv://dummyUser:dummyPassword123@cluster0.5skhm3x.mongodb.net/?retryWrites=true&w=majority",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  )
   .then(() => {
     app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
   })
   .catch((error) => console.log(`${error} did not connect`));
 
-  var statisticsRouter = require("./routes/statistics");
+// routes
+import statisticsRouter from "./routes/statistics.js";
+app.use("/", statisticsRouter);
--- backend/routes/statistics.js
+++ backend/routes/statistics.js
@@ -1,3 +1,10 @@
 import express from "express";
-
 const router = express.Router();
+
+// router.get("/", async function (req, res) {
+    
+// })
+
+router.get("/", (req, res) => res.status(200).json({message:"worked"}));
+
+export default router;
