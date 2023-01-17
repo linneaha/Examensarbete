033a2e87b17bf6eed7948de0755b3672e22a5792
Linnea <89969399+linneaha@users.noreply.github.com>
2023-01-17 11:53:54 +0100

    add put

--- backend/controllers/taskController.js
+++ backend/controllers/taskController.js
@@ -13,6 +13,13 @@ const createTask = async (req, res) => {
   res.status(200).json(task);
 };
 
+const updateTask = async(req,res) => {
+  // const task = await Task.findById(req.params.id);
+
+  const updatedTask = await Task.findByIdAndUpdate(req.params.id, req.body)
+
+  res.status(200).json(updatedTask);
+}
 
 const deleteTask = async (req, res) => {
   const task = await Task.findById(req.params.id);
@@ -20,4 +27,4 @@ const deleteTask = async (req, res) => {
   res.status(200).json({ id: req.params.id });
 };
 
-module.exports = { getTasks, createTask, deleteTask };
+module.exports = { getTasks, createTask, deleteTask, updateTask };
--- backend/routes/taskRoutes.js
+++ backend/routes/taskRoutes.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const router = express.Router();
-const { getTasks, createTask, deleteTask } = require("../controllers/taskController");
+const { getTasks, createTask, deleteTask, updateTask } = require("../controllers/taskController");
 
 router.get("/", getTasks);
 router.post("/", createTask);
-router.post("/", createTask);
+router.put("/:id", updateTask);
 router.delete("/:id", deleteTask);
 
 module.exports = router;
