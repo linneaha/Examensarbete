84c38fceab5bd7574ec5005354a0879a3f2c7068
Linnea <89969399+linneaha@users.noreply.github.com>
2023-01-26 10:32:50 +0100

    add logic to prevent crash if activity dont have stats

--- frontend/src/pages/Statistics.jsx
+++ frontend/src/pages/Statistics.jsx
@@ -12,42 +12,60 @@ const Statistics = () => {
   }, []);
 
   const convertTime = (num) => {
-    var minutes = Math.floor(num / 60000);
-    var seconds = ((num % 60000) / 1000).toFixed(0);
-    var hours = minutes % 60;
-    return (
-      hours +
-      (hours < 10 ? "0" : "") +
-      ":" +
-      minutes +
-      (minutes < 10 ? "0" : "") +
-      ":" +
-      (seconds < 10 ? "0" : "") +
-      seconds
-    );
+    if (num === 0) {
+      return "No data recorded!";
+    } else {
+      var minutes = Math.floor(num / 60000);
+      var seconds = ((num % 60000) / 1000).toFixed(0);
+      var hours = minutes % 60;
+      return (
+        hours +
+        (hours < 10 ? "0" : "") +
+        ":" +
+        minutes +
+        (minutes < 10 ? "0" : "") +
+        ":" +
+        (seconds < 10 ? "0" : "") +
+        seconds
+      );
+    }
   };
 
   const calculateAverageTime = (arr1, arr2) => {};
 
+  const capitalizeFirstLetter = (string) => {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  };
+
   let averageStatsForActivity = stats.map((stat) => ({
-    name: stat.name,
+    name: capitalizeFirstLetter(stat.name),
     averageActiveTime:
-      stat.stats
-        .map((stat) => stat.totalActiveTime)
-        .reduce((prev, curr) => prev + curr) / stat.stats.length,
+      stat.stats.length === 0
+        ? 0
+        : stat.stats
+            .map((stat) => stat.totalActiveTime)
+            .reduce((prev, curr) => prev + curr) / stat.stats.length,
     averageBreakTime:
-      stat.stats
-        .map((stat) => stat.totalBreakTime)
-        .reduce((prev, curr) => prev + curr) / stat.stats.length,
+      stat.stats.length === 0
+        ? 0
+        : stat.stats
+            .map((stat) => stat.totalBreakTime)
+            .reduce((prev, curr) => prev + curr) / stat.stats.length,
     averageTimeBeforeFirstBreak:
-      stat.stats
-        .map((stat) => stat.timeBeforeFirstBreak)
-        .reduce((prev, curr) => prev + curr) / stat.stats.length,
+      stat.stats.length === 0
+        ? 0
+        : stat.stats
+            .map((stat) => stat.timeBeforeFirstBreak)
+            .reduce((prev, curr) => prev + curr) / stat.stats.length,
     averageAmountOfBreaks:
-      stat.stats
-        .map((stat) => stat.amountOfBreaks)
-        .reduce((prev, curr) => prev + curr) / stat.stats.length,
+      stat.stats.length === 0
+        ? 0
+        : stat.stats
+            .map((stat) => stat.amountOfBreaks)
+            .reduce((prev, curr) => prev + curr) / stat.stats.length,
   }));
+
+  console.log(averageStatsForActivity);
 
   return (
     <div className="statsWrapper">
