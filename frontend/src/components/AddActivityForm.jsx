824ab429c8758cacc8771c3ed6c21b765bca54cb
Linnea <89969399+linneaha@users.noreply.github.com>
2023-01-31 11:43:02 +0100

    form styling

--- frontend/src/components/AddActivityForm.jsx
+++ frontend/src/components/AddActivityForm.jsx
@@ -28,15 +28,29 @@ const AddActivityForm = ({ toggleModal }) => {
       <div className="modalWrapper">
         <GrFormClose onClick={toggleModal} className="exitBtn" />
         <h1>Add new activity</h1>
-        <form className="addActivityForm" onSubmit={handleSubmit}>
-          <label htmlFor="name">Name</label>
-          <input
-            type="text"
-            id="name"
-            onChange={(e) => setName(e.target.value)}
-            value={name}
-          />
-          <button type="submit">+</button>
+        <form className="activityForm" onSubmit={handleSubmit}>
+          <div>
+            <label htmlFor="name">Name</label>
+            <input
+              type="text"
+              id="name"
+              onChange={(e) => setName(e.target.value)}
+              value={name}
+              required
+            />
+          </div>
+          <div>
+            <label>Choose icon</label>
+            <div className="iconGrid">
+              <img src="https://via.placeholder.com/50" alt="icon" />
+              <img src="https://via.placeholder.com/50" alt="icon" />
+              <img src="https://via.placeholder.com/50" alt="icon" />
+              <img src="https://via.placeholder.com/50" alt="icon" />
+              <img src="https://via.placeholder.com/50" alt="icon" />
+              <img src="https://via.placeholder.com/50" alt="icon" />
+            </div>
+          </div>
+          <button type="submit" className="addBtn">+</button>
         </form>
       </div>
     </Modal>
